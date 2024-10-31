9afd94d8bcc314e61416ca3ed3f9b4de82ce3590
bahjat <bahjat@homeeasy.com>
2024-11-01 00:56:19 +0500

    added total percentage and originated city and state of clients

--- building_send_clients.py
+++ building_send_clients.py
@@ -6,7 +6,6 @@ from datetime import datetime
 def may_update_channel_clients():
     st.title("Buildings Sent to Clients")
 
-    # Database connection parameters
     db_params = {
         'dbname': st.secrets["database"]["DB_NAME"],
         'user': st.secrets["database"]["DB_USER"],
@@ -19,7 +18,6 @@ def may_update_channel_clients():
 
     selected_datetime_start = f"{selected_date} 00:00:00"
     selected_datetime_end = f"{selected_date} 23:59:59"
-
     fetch_clients_query = f"""
         SELECT DISTINCT ON (c.id)
             c.id AS client_id,
@@ -28,10 +26,13 @@ def may_update_channel_clients():
             e.fullname AS employee_name,
             r.budget,
             r.beds,
-            r.move_in_date AT TIME ZONE 'UTC' AT TIME ZONE 'CST' AS move_in_date_ct,
+            r.move_in_date AT TIME ZONE 'UTC' AT TIME ZONE 'CST' AS move_in_date,
             r.credit_score,
             r.section8,
-            c.created AT TIME ZONE 'UTC' AT TIME ZONE 'CST' AS created_ct
+            c.created AT TIME ZONE 'UTC' AT TIME ZONE 'CST' AS created_at,
+            c.assigned_employee,
+            c.addresses->0->>'city' AS originating_city,
+            c.addresses->0->>'state' AS originating_state
         FROM 
             public.client c
         JOIN 
@@ -46,7 +47,6 @@ def may_update_channel_clients():
         ORDER BY 
             c.id, c.created;
     """
-
     def fetch_data(query):
         connection = None
         cursor = None
@@ -65,19 +65,24 @@ def may_update_channel_clients():
                 cursor.close()
             if connection:
                 connection.close()
-
     def display_clients_as_table(df):
         st.subheader(f"Clients on {selected_date}")
+        
         if df.empty:
             st.write("No clients found.")
         else:
+            total_clients = len(df)
+            not_assigned_to_317_318_319 = (~df['assigned_employee'].isin([317, 318, 319])).sum()
+            percentage_not_assigned = (not_assigned_to_317_318_319 / total_clients) * 100
+
+            # st.write(f"Percentage of clients assigned to employees: {percentage_not_assigned:.2f}%")
+            st.subheader(f"Percentage of clients assigned to employees: {percentage_not_assigned:.2f}%", divider="green")
+
             df['FUB Link'] = df.apply(lambda row: f'<a href="{row["followup_boss_link"]}" target="_blank">Go to Link</a>', axis=1)
-            df = df[['client_name', 'employee_name', 'budget', 'beds', 'move_in_date_ct', 'credit_score', 'section8', 'created_ct', 'FUB Link']]
+            df = df[['client_name', 'employee_name', 'budget', 'beds', 'move_in_date', 'credit_score', 'section8', 'created_at', 'originating_city', 'originating_state', 'FUB Link']]
 
             st.write(df.to_html(escape=False), unsafe_allow_html=True)
 
     clients_data = fetch_data(fetch_clients_query)
 
     display_clients_as_table(clients_data)
-
-# may_update_channel_clients()
