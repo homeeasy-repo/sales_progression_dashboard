c35d1757d8c4a35f0057cb600253492a5e09a16a
bahjat <bahjat@homeeasy.com>
2024-12-10 22:25:29 +0500

    added beds and baths

--- client_process_sold.py
+++ client_process_sold.py
@@ -23,7 +23,7 @@ def show_responsive_clients():
     start_datetime = datetime.combine(start_date, datetime.min.time())
     end_datetime = datetime.combine(end_date, datetime.max.time())
 
-    # Query to fetch responsive clients
+    # Query to fetch responsive clients with beds and baths
     fetch_responsive_clients_query = f"""
     WITH clients_created_today AS (
         SELECT 
@@ -34,6 +34,8 @@ def show_responsive_clients():
             c.fullname AS client_name,
             r.move_in_date AS move_in_date,
             r.budget AS budget,
+            r.beds AS beds,
+            r.baths AS baths,
             r.section8 AS section_8,
             r.credit_score AS credit_score,
             (c.addresses->0->>'city') AS city,
@@ -62,11 +64,13 @@ def show_responsive_clients():
         c.client_name,
         CONCAT('https://services.followupboss.com/2/people/view/', c.client_id) AS followup_boss_link,
         c.assigned_employee_name AS employee_name,
+        c.phone_number,
         c.budget,
+        c.beds,
+        c.baths,
         c.move_in_date,
         c.credit_score,
         c.section_8 AS section8,
-        c.phone_number AS phone_number,
         c.city,
         c.state,
         c.street
@@ -106,13 +110,11 @@ def show_responsive_clients():
             df['FUB Link'] = df.apply(
                 lambda row: f'<a href="{row["followup_boss_link"]}" target="_blank">Go to Link</a>', axis=1
             )
-            df = df[['client_name', 'employee_name', 'phone_number', 'budget', 'move_in_date', 
-                     'credit_score', 'section8', 'city', 'state', 'street', 'FUB Link']]
+            df = df[['client_name', 'employee_name', 'phone_number', 'budget', 'beds', 'baths', 
+                     'move_in_date', 'credit_score', 'section8', 'city', 'state', 'street', 'FUB Link']]
             
             st.write(df.to_html(escape=False), unsafe_allow_html=True)
 
     # Fetch and display the data
     responsive_clients_data = fetch_data(fetch_responsive_clients_query)
     display_clients_as_table(responsive_clients_data)
-
-# show_responsive_clients()
